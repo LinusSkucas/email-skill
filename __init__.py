1e6b6be2f27f45755739420e2851d1c0cbdaf617
Linus <LinusS1@users.noreply.github.com>
2018-12-30 21:39:20 -0600

    Add prettyness to reporting

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Linus S
+# Copyright 2019 Linus S
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,53 +19,14 @@ import imaplib
 import email
 import email.header
 import email.utils
+import inflect
 
 EMAIL_POLL_INTERVAL = 120 # in seconds
-
-def list_new_email(account, folder, password, port, address, whitelist = None, mark_as_seen = False):
-    """
-    Returns new emails.
-    output:
-    dicts in the format :{name,sender,subject}
-    whitelist: the a list of emails that it will return
-    """
-    M = imaplib.IMAP4_SSL(str(address), port=int(port))
-    M.login(str(account), str(password))#Login
-    M.select(str(folder))
-    rv, data = M.search(None, "(UNSEEN)")#Only get unseen/unread emails
-    message_num = 1
-    new_emails = []
-    for num in data[0].split():
-        rv, data = M.fetch(num, '(RFC822)')
-        msg = email.message_from_bytes(data[0][1])
-        hdr = email.header.make_header(email.header.decode_header(msg['Subject']))#Get subject
-        sender = email.header.make_header(email.header.decode_header(msg['From']))#Get sender
-        from_email = email.utils.parseaddr(msg['From'])[1]
-        subject = str(hdr)
-        sender = str(sender)
-
-        is_in_whitelist = not whitelist or from_email in whitelist or any(s.lower() in sender.lower().split(" ") for s in whitelist)
-        if is_in_whitelist and mark_as_seen:
-            M.store(num, "+FLAGS", '\\SEEN')
-        else:
-            M.store(num, "-FLAGS", '\\SEEN') #Some email providers automaticly mark a message as seen: undo that.
-        mail = {"message_num": message_num, "sender": sender, "subject": subject}
-        if not is_in_whitelist:
-            # The user does not want emails from that sender, skip it
-            continue
-        
-        new_emails.append(mail)
-        message_num += 1
-    #Clean up
-    M.close()
-    M.logout()
-
-    return new_emails
 
 def normalize_email(email):
     if not email:
         return None
-    
+
     result = ""
 
     for token in email.split():
@@ -85,16 +46,18 @@ class Email(MycroftSkill):
     def __init__(self):
         """Init"""
         MycroftSkill.__init__(self)
-        #SETup the passwords
 
+        self.i_engine = inflect.engine()
+
+        #SETup the passwords
         self.account = self.settings.get('username')
         self.password = self.settings.get('password')
-        server = self.settings.get("server")
-        if account == None or account == "" or server == None or server == "":
+        self.server = self.settings.get("server")
+        if self.account is None or self.account == "" or self.server is None or self.server == "":
             self.config = self.config_core.get("email_login", {})
-            self.account = config.get("email")#Get settings in config file
-            self.password = config.get("password")
-            if account == None or account == "" or password == "" or password == None:
+            self.account = self.config.get("email")#Get settings in config file
+            self.password = self.config.get("password")
+            if self.account is None or self.account == "" or self.password == "" or self.password is None:
                 #Not set up in file/home
                 self.speak_dialog("setup")
                 return
@@ -107,7 +70,11 @@ class Email(MycroftSkill):
         if self.settings.get('look_for_mail'):
             self.schedule_repeating_event(self.poll_emails, datetime.now(), EMAIL_POLL_INTERVAL, name='poll.emails')
 
+    def _nice_number(self, num):
+        return self.i_engine.number_to_words(self.i_engine.ordinal(num))
+
     def report_email(self, new_emails):
+        """Report and say the email"""
         stop_num = 10
         #report back
         for x in range(0, len(new_emails)):
@@ -123,11 +90,56 @@ class Email(MycroftSkill):
                    stop_num += 10
                    continue
 
+    def list_new_email(self, account, folder, password, port, address, whitelist=None, mark_as_seen=False):
+        """
+        Returns new emails.
+        output:
+        dicts in the format :{name,sender,subject}
+        whitelist: the a list of emails that it will return
+        """
+        M = imaplib.IMAP4_SSL(str(address), port=int(port))
+        M.login(str(account), str(password))  # Login
+        M.select(str(folder))
+        rv, data = M.search(None, "(UNSEEN)")  # Only get unseen/unread emails
+        message_num = 1
+        new_emails = []
+        for num in data[0].split():
+            rv, data = M.fetch(num, '(RFC822)')
+            msg = email.message_from_bytes(data[0][1])
+            hdr = email.header.make_header(email.header.decode_header(msg['Subject']))  # Get subject
+            sender = email.header.make_header(email.header.decode_header(msg['From']))  # Get sender
+            from_email = email.utils.parseaddr(msg['From'])[1]
+            subject = str(hdr)
+            sender = str(sender)
+
+            is_in_whitelist = not whitelist or from_email in whitelist or any(
+                s.lower() in sender.lower().split(" ") for s in whitelist)
+            if is_in_whitelist and mark_as_seen:
+                M.store(num, "+FLAGS", '\\SEEN')
+            else:
+                M.store(num, "-FLAGS", '\\SEEN')  # Some email providers automaticly mark a message as seen: undo that.
+            #For english ordianals TODO: I should localize this
+            if self.lang == "en-us":
+                mail = {"message_num": self._nice_number(message_num), "sender": sender, "subject": subject}
+            else:
+                mail = {"message_num": message_num, "sender": sender, "subject": subject}
+            if not is_in_whitelist:
+                # The user does not want emails from that sender, skip it
+                continue
+
+            new_emails.append(mail)
+            message_num += 1
+        # Clean up
+        M.close()
+        M.logout()
+
+        return new_emails
+
     def poll_emails(self, data):
         setting = self.settings.get('look_for_email')
         #check email
         try:
-            new_emails = list_new_email(account=self.account, folder=self.folder, password=self.password, port=self.port, address=self.server, whitelist = setting['whitelist'], mark_as_seen = True)
+            new_emails = self.list_new_email(account=self.account, folder=self.folder, password=self.password, port=self.port, address=self.server, whitelist = setting['whitelist'], mark_as_seen = True)
         except Exception as e:
             # Silently ignore errors
             return
@@ -135,13 +147,13 @@ class Email(MycroftSkill):
         if not new_emails:
             # No new mail
             return
-        
+
         stop_num = 10
         num_emails = len(new_emails)
         response = self.ask_yesno(prompt="notify.read.email", data={"size" : num_emails})
         if response != "yes": #localize
             return
-        
+
         self.report_email(new_emails)
 
     @intent_file_handler('enquire.email.intent')
@@ -151,7 +163,7 @@ class Email(MycroftSkill):
         setting = self.settings.get('look_for_email')
         #check email
         try:
-            new_emails = list_new_email(account=self.account, folder=self.folder, password=self.password, port=self.port, address=self.server, whitelist = [sender], mark_as_seen = True)
+            new_emails = self.list_new_email(account=self.account, folder=self.folder, password=self.password, port=self.port, address=self.server, whitelist = [sender], mark_as_seen = True)
         except Exception as e:
             # Silently ignore errors
             return
@@ -163,7 +175,7 @@ class Email(MycroftSkill):
 
         self.report_email(new_emails)
 
-            
+
     @intent_file_handler('notify.intent')
     def enable_email_polling(self, message):
         sender = normalize_email(message.data.get('sender'))
@@ -174,7 +186,7 @@ class Email(MycroftSkill):
                 self.settings['look_for_email'] = { "whitelist" : [sender] }
             else:
                 self.settings['look_for_email'] = { "whitelist" : None }
-                
+
             # Start the notification service
             self.schedule_repeating_event(self.poll_emails, datetime.now(), EMAIL_POLL_INTERVAL, name='poll.emails')
             self.speak_dialog("start.poll.email")
@@ -205,7 +217,7 @@ class Email(MycroftSkill):
                     return
             else:
                 setting['whitelist'].append(sender)
-            
+
             self.speak_dialog("update.notify.data")
         self.settings.store()
 
@@ -244,15 +256,15 @@ class Email(MycroftSkill):
                 self.settings['look_for_email'] = None
                 self.remove_event('poll.emails')
                 self.speak_dialog("stop.poll.emails.last.email.removed")
-        
+
         self.settings.store()
-            
+
     @intent_file_handler('check.email.intent')
     def handle_email(self, message):
        """Get the new emails and speak it"""
        #check email
        try:
-           new_emails = list_new_email(account=self.account, folder=self.folder, password=self.password, port=self.port, address=self.server)
+           new_emails = self.list_new_email(account=self.account, folder=self.folder, password=self.password, port=self.port, address=self.server)
        except Exception as e:
            #Error? give an error
            self.speak_dialog("error.getting.mail")
